d4bb57d27578be548b7a0b8c2f8750b69e924e5e
Andrés Felipe García Granados <garciapipe1994@gmail.com>
2021-01-01 22:47:58 -0500

    se agrega implementación para SpecialtiesService

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
